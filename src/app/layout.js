a13f25784fad0c3932289437b33798bdd268d8c8
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-05-22 12:17:07 +0600

    added a responsive footer with links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,8 +25,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar/>
+        <Navbar />
         <main className="max-w-7xl mx-auto">{children}</main>
+        <Footer />
       </body>
     </html>
   );
