a1ccf74930cde97e19113b35a3705e013f9e4914
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-05-27 15:41:34 +0600

    Restore GitHub streak

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,5 @@
 import { betterAuth } from 'better-auth';
+import { jwt } from 'better-auth/plugins';
 import { MongoClient } from 'mongodb';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 
@@ -18,5 +19,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
+  plugins: [jwt()],
 });
 
